dccbc0c409ef7372cf676b339c707e6c98b95a52
Administrator <cunkuan.ding@tenbent.com>
2017-09-26 15:37:40 +0800

    user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,6 +4,9 @@ var router = express.Router();
 
 var libs = process.cwd() + '/routes/';
 var log = require(libs + 'log')(module);
+var bcrypt = require('bcrypt');
+
+var SALT_WORK_FACTOR = 10;
 
 var User = require("./schema/user.js");          //schema User
 
@@ -39,17 +42,8 @@ router.post('/createuser', function (req, res, next) {
             });
         }else if(is_old){
             var data = {
-                username: req.body.username,
                 password: req.body.password,
                 phone: req.body.phone,
-                e_mail: req.body.e_mail,
-                age: req.body.age,
-                birthday: req.body.birthday,
-                gender: req.body.gender,
-                address: req.body.address,
-                head: req.body.head,
-                logindate: new Date(),
-                createdate: new Date()
             };
             var user = new User(data);
             user.save(function (err, result) {
@@ -75,6 +69,21 @@ router.post('/createuser', function (req, res, next) {
 });
 
 
+
+function IsUser(password, hash, callback) {
+    bcrypt.compare(password, hash, function(err, IsUserData) {
+        if(err){
+            log.debug('连接服务错误');
+            callback('500')
+        } else {
+            if (IsUserData) {
+                callback(true)
+            } else {
+                callback(false)
+            }
+        }
+    });
+}
 /**
  * 登录用户信息
  */
@@ -89,17 +98,25 @@ router.post('/login', function (req, res, next) {
             });
         } else {
             if (result) {
-                if (result.password === password) {
-                    res.json({
-                        code: '0',
-                        data: result
-                    });
-                } else {
-                    res.json({
-                        code: '10002',
-                        msg: "密码错误"
-                    });
-                }
+                IsUser(password, result.password, function (code) {
+                    if (code === '500') {
+                        res.json({
+                            code: '500',
+                            msg: "服务器错误"
+                        });
+                    } else if (code) {
+                        delete result.password;
+                        res.json({
+                            code: '0',
+                            msg: "ok"
+                        });
+                    } else {
+                        res.json({
+                            code: '10002',
+                            msg: "密码错误"
+                        });
+                    }
+                });
             } else {
                 res.json({
                     code: '10001',
@@ -192,26 +209,27 @@ router.put('/userupdate', function (req, res, next) {
 //
 //
 //
-// /**
-//  * 全部分页查询用户列表
-//  */
-// router.get('/userlist', function (req, res, next) {
-//     var page = parseInt(req.query.page);
-//     var size = parseInt(req.query.size);
-//     User.find({}).skip(page * size).limit(size).exec(function(err,result){
-//         if (err) {
-//             res.json({
-//                 code: '110',
-//                 msg: "Error:" + err
-//             });
-//         } else {
-//             res.json({
-//                 code: '100',
-//                 data: result
-//             });
-//         }
-//     });
-// });
+
+/**
+ * 全部分页查询用户列表
+ */
+router.get('/userlist', function (req, res, next) {
+    var page = parseInt(req.query.page) || 0;
+    var size = parseInt(req.query.size) || 10;
+    User.find({},{ password: 0 }).skip(page * size).limit(size).sort({createdate: -1}).exec(function(err,result){
+        if (err) {
+            res.json({
+                code: '500',
+                msg: "Error:" + err
+            });
+        } else {
+            res.json({
+                code: '0',
+                data: result
+            });
+        }
+    });
+});
 
 
 module.exports = router;
