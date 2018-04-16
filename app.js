a4f1cce05a2d6759d923c71707cc4ac958c29bea
Administrator <cunkuan.ding@tenbent.com>
2018-04-16 10:56:05 +0800

    ../

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+
 require('./routes/auth');
 
 var filter = require("./routes/filter");
